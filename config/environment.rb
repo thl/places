f51fcd46e2119c7dc19ad3472c7d13f83067e807
Andres Montano <amontano@virginia.edu>
2011-09-09 13:27:37 -0600

    updating rails to 2.3.8

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 APP_DOMAIN = "places.thlib.org"
 APP_NAME = "THL Places"
