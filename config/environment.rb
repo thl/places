81f500451ec78fbf38cf22abd1bc9c953c612a41
Andres Montano <amontano@virginia.edu>
2011-12-03 17:59:13 -0600

    updating engines

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,15 @@
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
+require File.join(File.dirname(__FILE__), '..', 'vendor', 'plugins', 'thl_integration', 'lib', 'thl_cookie')
 
 Rails::Initializer.run do |config|
   # config.gem 'fiveruns_tuneup'
   config.gem 'after_commit'
   config.plugins = [:acts_as_family_tree, :tiny_mce, :exception_notification, :places_engine, :all ]
   config.action_controller.session = {
-    :session_key => 'thl_session',
-    :secret      => '92c58b72c968a9b8699f7f0e41491e82e4a1d3d671c628f880e1d6409e5eb4f2b4c7598f6544c5a7b96d175ecba6fac80b3d1507b735d7f710e13ffdb65b2842'
+    :session_key => ThlCookie.session_key,
+    :secret      => ThlCookie.session_secret
   }
   # config.active_record.observers = :cached_category_count_sweeper
 end
