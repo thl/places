a5135d1aba79673b36d7728198e43daba1b4239f
Andres Montano <amontano@virginia.edu>
2012-05-16 17:40:31 -0600

    updating engines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,8 @@ Places::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  config.cache_store = :file_store #, File.join(RAILS_ROOT, 'tmp', 'cache')
+  # config.cache_store = :file_store #, File.join(RAILS_ROOT, 'tmp', 'cache')
+  
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
