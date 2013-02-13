7deeef85743c222164121461538b4c417f2c5049
Andres Montano <amontano@virginia.edu>
2013-02-13 15:51:08 -0600

    adding authenticated system migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,16 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111124195857) do
+ActiveRecord::Schema.define(:version => 20130213214759) do
 
   create_table "Birth_rate", :id => false, :force => true do |t|
     t.integer "gid",                       :null => false
@@ -588,7 +589,7 @@ ActiveRecord::Schema.define(:version => 20111124195857) do
 
   add_index "shapes", ["fid"], :name => "shapes_fid"
   add_index "shapes", ["geometry"], :name => "features_geometry_gist", :spatial => true
-  add_index "shapes", [nil], :name => "shapes_geotype"
+  add_index "shapes", [], :name => "shapes_geotype"
 
   create_table "simple_chinese", :primary_key => "gid", :force => true do |t|
     t.integer  "fid"
@@ -726,13 +727,14 @@ ActiveRecord::Schema.define(:version => 20111124195857) do
     t.string   "login",                                   :null => false
     t.string   "email",                                   :null => false
     t.integer  "person_id",                               :null => false
-    t.string   "crypted_password",          :limit => 40, :null => false
+    t.string   "crypted_password",          :limit => 40
     t.string   "salt",                      :limit => 40
     t.string   "remember_token"
     t.datetime "remember_token_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "identity_url"
+    t.string   "shibboleth_id"
   end
 
   create_table "xml_documents", :force => true do |t|
